532ef1958f62cf2bca094effbbe4c3965d59b0b8
jonathan.cruz <jonathan.cruz@smarttbotter.com.br>
2018-08-14 11:49:50 -0300

    Estrutura inicial para testes usando mocha e chai

---
+++ tests/main.spec.js
@@ -0,0 +1,18 @@
+describe('Main', () => {
+  describe('Metodo A', () => {
+    context('Case 1', () => {
+      it('should happen blablabla', () => {
+        // espera que aconteça
+        // entrada de dados / metodo sum(2,2)
+        // espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+    context('Case 2', () => {
+
+    });
+  });
+
+  describe('Metodo B', () => {
+
+  });
+});
