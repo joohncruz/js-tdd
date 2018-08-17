4f7badcee8cc26ab50fe47ec77a8bcca94b50af2
jonathan.cruz <jonathan.cruz@smarttbotter.com.br>
2018-08-17 18:59:43 -0300

    Usando expect

--- tests/hooks-mocha-example.spec.js
+++ tests/hooks-mocha-example.spec.js
@@ -1,48 +1,36 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
 describe('Hooks Mocha Example', () => {
-  let arr = [1, 2, 3];
-
-  // Executar antes de cada bloco, executa uma vez.
-  before(() => {
-    // inicia uma conexao no banco
-    // criar um conjunto de dados
-  });
-
-  // roda uma vez, depois do bloco
-  after(() => {
-    // fecha uma conexao do banco
-    // apagar esse conjunto de dados
-  });
+  let arr;
 
   // executa todas as vezes, depois de cada bloco
   beforeEach(() => {
     arr = [1, 2, 3];
   });
 
-  // roda todas as vezes, depois de cada bloco
-  afterEach(() => {
-
+  // testar tipos ou se existe (smoke test)
+  it('should be an array', () => {
+    expect(arr).to.be.a('array');
   });
 
   it('should have a size of 4 when push another value to the array', () => {
     arr.push(4);
-    console.log(arr.length); // 4
+    expect(arr).to.have.lengthOf(4);
   });
 
   it('should have a size of 2 when pop a value from the array', () => {
     arr.pop();
-    console.log(arr.length); // 2
+    expect(arr).to.have.lengthOf(2);
   });
 
   it('should remove the value 3 when use pop in the array', () => {
-    console.log(arr.pop() === 3); // true
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return true when pop 3 the array', () => {
+    expect(arr.pop() === 3).to.be.true;
   });
 });
-
-// before
-// beforeEach
-// test 1
-// afterEach
-// beforeEach
-// test 2
-// afterEach
-// after
