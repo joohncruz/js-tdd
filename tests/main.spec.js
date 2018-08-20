6b2f17dba48ba5531f0bfc8d9bf5f0ebda7c960d
jonathan.cruz <jonathan.cruz@smarttbotter.com.br>
2018-08-20 16:53:16 -0300

    Criando tdd do metodo fizzbuzz

---
+++ tests/main.spec.js
@@ -0,0 +1,36 @@
+/*
+
+Desafio FizzBuzz
+
+Escreva uma lib que receba um número e:
+
+Se o número for divisivel por 3, no lugar do número escreva "Fizz" - X
+Se o número for divisivel por 5, no lugar do número escreva "Buzz" - X
+Se o número for divisivel por 3 e 5, no lugar do número escreva "FizzBuzz"
+Se não for múltiplo de nada, retorne o número
+
+*/
+
+import { expect } from 'chai';
+import FizzBuzz from '../src/main';
+
+describe('Main', () => {
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  });
+
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
+  });
+
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+  });
+
+  it('should return the number when nom-multiple of 3 and 5', () => {
+    expect(FizzBuzz(2)).to.be.equal(2);
+    expect(FizzBuzz(7)).to.be.equal(7);
+  });
+});
